3939785ac69b4cf6852c7189ceefdd9ef06f5054
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-25 12:24:41 +0800

    chore: cdnjs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 var postRouter = require("./routes/posts");
 
 const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css";
   
 const options = {
   definition: {
