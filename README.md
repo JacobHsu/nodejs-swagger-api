cb9f10ef7468e9d445de473b7e072f7f7f70f00c
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-25 10:11:18 +0800

    feat: /posts

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 expressjs [installing](https://expressjs.com/en/starter/installing.html)  
 [Hello world example](https://expressjs.com/en/starter/hello-world.html) 
+
+
+[Postman](https://www.postman.com/): The World's Leading API Platform | Sign Up for Free  
--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var postRouter = require("./routes/posts");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/posts", postRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
